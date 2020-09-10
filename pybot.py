60abdb1bd895908270374429428457fb63a9223c
Nicole White <nicci498@gmail.com>
2020-09-10 13:00:45 -1000

    creds in?

--- pybot.py
+++ pybot.py
@@ -1,2 +1,4 @@
 import praw
 import json
+
+reddit = praw.Reddit(client_id = 'FhCZAoozfF60nQ', client_secret = 'qT_sfXUwNI5IIVgoqSAgpEKaWUo', user_agent = 'rougndraft', username = 'BrightnessShallanVin' password = 'Waterband')
